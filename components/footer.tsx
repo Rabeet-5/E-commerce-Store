91b8c74848a3892f3ba3026c2d20d558b5278c82
Rabeet-5 <rabeetsati@gmail.com>
2023-10-07 03:58:56 +0500

    Navbar ...

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-white border-t '>
+        <div className='mx-auto py-10'>
+            <p className='text-center text-sm text-black font-semibold'>
+            &copy; 2023 E-commerce , Inc. All rights reserved.
+            </p>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
