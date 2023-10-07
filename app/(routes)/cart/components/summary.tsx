32fa044141f0604ac406af25799be649f4cabd68
Rabeet-5 <rabeetsati@gmail.com>
2023-10-08 04:03:11 +0500

    commit (store)

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -8,7 +8,6 @@ import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
-import { stat } from "fs";
 
 const Summary = () => {
   
@@ -51,7 +50,7 @@ const Summary = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} className="w-full mt-6">
+      <Button disabled={items.length === 0} onClick={onCheckout} className="w-full mt-6">
         Checkout
       </Button>
     </div>
