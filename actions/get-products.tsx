7d9117a606855bc79fdcb296f554f34dc1935b1f
Rabeet-5 <rabeetsati@gmail.com>
2023-10-09 04:31:45 +0500

    fixed some errors

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,36 +1,30 @@
-
-import qs from "query-string"
+import qs from "query-string";
 
 import { Product } from "@/types";
 
-
-
 interface Query {
-    categoryId?: string;
-    colorId?: string;
-    sizeId?: string;
-    isFeatured?: boolean;
-  }
-
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+  isFeatured?: boolean;
+}
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProducts = async (query:Query) : Promise<Product[]> => {
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
+      isFeatured: query.isFeatured,
+    },
+  });
 
-    const url = qs.stringifyUrl({
-        url: URL,
-        query: { 
-          colorId: query.colorId,
-          sizeId: query.sizeId,
-          categoryId: query.categoryId,
-          isFeatured: query.isFeatured,
-        },
-      });
+  const res = await fetch(url);
 
-    const res = await fetch(url);
+  return res.json();
+};
 
-    return res.json();
-}
-
-
-export default getProducts
+export default getProducts;
